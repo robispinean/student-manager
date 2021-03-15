60641dfe265d17e77d535653b1a95cd00766c57b
Robert Spinean <robispinean@yahoo.com>
2021-03-15 19:20:59 +0200

    SM-2 Implemented the Student model class

---
+++ src/student_manager/Student.java
@@ -0,0 +1,33 @@
+package student_manager;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double meanGrade;
+
+    public Student(String firstName, String lastName, int age, double meanGrade) {
+        //assignments
+    }
+
+    @Override
+    public String toString() {
+        //toString generated implementation
+        return "";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        //equals generated implementation
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        //hashCode generated implementation
+        return 0;
+    }
+
+    //generated getters and setters for all fields
+
+}
